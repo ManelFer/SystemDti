d114d64b16103692ed7559aa2e19abbd35e4894f
Manoel Ferreira Matos <manoelferreiramatos08@gmail.com>
2024-08-07 15:34:19 -0300

    mudança
    Feita mudança no front do sistema

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
-// Seleciona os elementos do formulário
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
 const loginButton = document.getElementById("login-button");
+const cartModal = document.getElementById("cart-modal");
+const closerModalBtn = document.getElementById("closer-modal-btn");
+const checkoutBtn = document.getElementById("checkout-btn");
 
-// Função para validar o formulário
-function validateForm() {
-  const email = emailInput.value.trim();
-  const password = passwordInput.value.trim();
+// abrir modal
+loginButton.addEventListener("click", function () {
+  cartModal.style.display = "flex";
+});
 
-  if (email === "" || password === "") {
-    alert("Preencha todos os campos!");
-    return false;
-  }
-
-  // Aqui você pode adicionar mais validações, como verificar se o e-mail é válido, etc.
-  if (email === "manoelferreiramatos.ti@gmail.com" || password === "1234") {
-    alert("Bem vindo" + email);
-  }else if(email != "manoelferreiramatos.ti@gmail.com" || password != "1234" ){
-    alert("Senha ou e-mail não cadastrado!");
-    return false
-  }
-
-  return true;
-}
-
-// Função para realizar o login
-function login() {
-  if (validateForm()) {
-    // Aqui você pode adicionar a lógica para realizar o login, como enviar uma requisição para um servidor, etc.
-    window.location.href = "./frontend/dash.html";
-  }
-}
-
-// Adiciona o evento de click ao botão de login
-loginButton.addEventListener("click", login);
+// fechar modal apertando voltar
+closerModalBtn.addEventListener("click", function () {
+  cartModal.style.display = "none";
+});
